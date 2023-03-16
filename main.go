72276a8dabd932aa69c587aadba7a24ccbfd7b64
NaN Noda <nananannoda@gmail.com>
2023-03-16 14:07:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
+	"image/jpeg"
+	"os"
 
 	"github.com/pion/mediadevices"
-	_ "github.com/pion/mediadevices/pkg/driver/microphone"
+	"github.com/pion/mediadevices/pkg/prop"
 	utils "nannnoda.com/pngtuber/internal"
 )
 
 // import signal.go from the same directory as this file
 
 func main() {
-
-	stream, err := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
-		Audio: func(c *mediadevices.MediaTrackConstraints) {
-
+	stream, _ := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
+		Video: func(constraint *mediadevices.MediaTrackConstraints) {
+			// Query for ideal resolutions
+			constraint.Width = prop.Int(600)
+			constraint.Height = prop.Int(400)
 		},
 	})
-	if err != nil {
-		panic(err)
-	}
-	track := stream.GetAudioTracks()[0]
-	audioTrack := track.(*mediadevices.AudioTrack)
-	defer audioTrack.Close()
 
-	audioReader := audioTrack.NewReader(false)
+	// Since track can represent audio as well, we need to cast it to
+	// *mediadevices.VideoTrack to get video specific functionalities
+	track := stream.GetVideoTracks()[0]
+	videoTrack := track.(*mediadevices.VideoTrack)
+	defer videoTrack.Close()
 
-	chunk, release, _ := audioReader.Read()
+	// Create a new video reader to get the decoded frames. Release is used
+	// to return the buffer to hold frame back to the source so that the buffer
+	// can be reused for the next frames.
+	videoReader := videoTrack.NewReader(false)
+	frame, release, _ := videoReader.Read()
 	defer release()
 
-	fmt.Println(chunk.ChunkInfo())
+	// Since frame is the standard image.Image, it's compatible with Go standard
+	// library. For example, capturing the first frame and store it as a jpeg image.
+	output, _ := os.Create("frame.jpg")
+	jpeg.Encode(output, frame, nil)
 
 	remoteIdChan := make(chan string)
 	localIdChan := make(chan string)
